8fd76a642f78c53ff1fbb730aedebe14bc9da1c6
Manoj <manojd.official@gmail.com>
2025-12-07 21:04:39 +0530

    Backend for Deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "http://localhost",
+        "http://localhost","https://zen-ai-referral-fax-extractor.vercel.app/upload"
     ],
     allow_credentials=True,
     allow_methods=["*"],
